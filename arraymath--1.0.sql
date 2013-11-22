869b70c9c3a27cc565bd02a663c7d16aaab9443f
Paul Ramsey <pramsey@opengeo.org>
2013-11-22 13:53:45 -0800

    Buildable draft of array-on-array anytype magic

--- arraymath--1.0.sql
+++ arraymath--1.0.sql
@@ -1,20 +1,8 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION http" to load this file. \quit
+\echo Use "CREATE EXTENSION arraymath" to load this file. \quit
 
-CREATE TYPE http_response AS (
-	status INTEGER,
-	content_type VARCHAR,
-	headers VARCHAR,
-	content VARCHAR
-);
-
-CREATE OR REPLACE FUNCTION http_get(url VARCHAR, params VARCHAR DEFAULT NULL)
-	RETURNS http_response
+CREATE OR REPLACE FUNCTION array_add(arr1 ANYARRAY, arr2 ANYARRAY)
+	RETURNS integer
 	AS 'MODULE_PATHNAME'
 	LANGUAGE C;
 
-CREATE OR REPLACE FUNCTION urlencode(string VARCHAR)
-	RETURNS text
-	AS 'MODULE_PATHNAME'
-	LANGUAGE C
-	IMMUTABLE STRICT;
--- arraymath.c
+++ arraymath.c
@@ -29,11 +29,23 @@
 
 #define ARRAYMATH_VERSION "1.0"
 
+
 /* PostgreSQL */
 #include <postgres.h>
 #include <fmgr.h>
 #include <funcapi.h>
 #include <utils/builtins.h>
+#include <utils/array.h>
+#include <catalog/pg_operator.h>
+#include <catalog/namespace.h>
+#include <utils/syscache.h>
+#include <utils/typcache.h>
+#include <nodes/value.h>
+
+
+/**********************************************************************
+* PostgreSQL initialization routines
+*/
 
 /* Set up PgSQL */
 PG_MODULE_MAGIC;
@@ -54,52 +66,292 @@ void _PG_fini(void)
 }
 
 
-
-/**
-* Our only function, takes in two arrays and tries to add
-* them together.
+/**********************************************************************
+* Utility macros
 */
-Datum array_add(PG_FUNCTION_ARGS);
-PG_FUNCTION_INFO_V1(array_add);
-Datum array_add(PG_FUNCTION_ARGS)
+
+#define ARRISEMPTY(x)  (ARRNELEMS(x) == 0)
+
+#define BITMAP_GET(bitmap, i) (bitmap && (bitmap[(i)/sizeof(bits8)] & (1 << ((i) % sizeof(bits8)))))
+
+#define BITMAP_INCREMENT(bitmap, bitmask) do { \
+    bitmask <<= 1; \
+    if (bitmask == 0x100) { \
+        bitmap++; bitmask = 1; \
+    } } while(0);
+
+#define BITMAP_ISNULL(bitmap, bitmask) (bitmap && (*bitmap & bitmask) == 0)
+    
+
+
+/**********************************************************************
+* Functions
+*/
+    
+
+/*
+* Given an operator symbol ("+", "-", "=" etc) and type element types, 
+* try to look up the appropriate function to do element level operations of 
+* that type.
+*/
+static void 
+arraymath_fmgrinfo_from_optype(const char *opstr, Oid element_type1, 
+                               Oid element_type2, FmgrInfo *operfmgrinfo)
 {
-    ArrayType  *array1 = PG_GETARG_ARRAYTYPE_P(0);
-    ArrayType  *array2 = PG_GETARG_ARRAYTYPE_P(1);
-    Oid collation = PG_GET_COLLATION();
+    Oid operator_oid;    
+    HeapTuple opertup;
+    Form_pg_operator operform;
+
+    /* Look up the operator Oid that corresponts to this combination */
+    /* of symbol and data types */
+    operator_oid = OpernameGetOprid(list_make1(makeString(pstrdup(opstr))), element_type1, element_type2);
+    if ( ! (operator_oid && OperatorIsVisible(operator_oid)) )
+    {
+        elog(ERROR, "operator does not exist");
+    }
+
+    /* Lookup the function associated with the operator Oid */
+    opertup = SearchSysCache1(OPEROID, ObjectIdGetDatum(operator_oid));
+    if (! HeapTupleIsValid(opertup))
+    {
+        elog(ERROR, "cannot find operator heap tuple");
+    }
+
+    operform = (Form_pg_operator) GETSTRUCT(opertup);
+
+    fmgr_info(operform->oprcode, operfmgrinfo);
+    ReleaseSysCache(opertup);
+
+    return;
+}
+
+/*
+* Apply an operator using an element over all the elements 
+* of an array.
+*/
+static ArrayType * 
+arraymath_array_oper_elem(ArrayType *array1, char *opname, ArrayType *array2)
+{
+    int ndims1 = ARR_NDIM(array1);
+    int *dims1 = ARR_DIMS(array1);
+    Oid element_type1 = ARR_ELEMTYPE(array1);
+    int nitems1;
+    FmgrInfo operfmgrinfo;
+    
+    elog(ERROR, "not implemeted yet");
+    return NULL;
+}
+
+/*
+* Read type information for a given element type.
+*/
+static TypeCacheEntry * 
+arraymath_typentry_from_type(Oid element_type)
+{
+    TypeCacheEntry *typentry;
+    typentry = lookup_type_cache(element_type, 0);
+    if (!typentry)
+    {
+        elog(ERROR, "unable to lookup element type info for %s", format_type_be(element_type));
+    }
+    return typentry;    
+}
+
+
+
+/*
+* Apply an operator over all the elements of a pair of arrays
+* expanding to return an array of the same size as the largest
+* input array.
+*/
+static ArrayType * 
+arraymath_array_oper_array(ArrayType *array1, char *opname, ArrayType *array2)
+{
+    ArrayType *array_out;
+    int	dims[1];
+	int	lbs[1];
+    Datum *elems;
+    bool *nulls;
+	
     int ndims1 = ARR_NDIM(array1);
     int ndims2 = ARR_NDIM(array2);
     int *dims1 = ARR_DIMS(array1);
     int *dims2 = ARR_DIMS(array2);
+    char *ptr1, *ptr2;
     Oid element_type1 = ARR_ELEMTYPE(array1);
-    Oid element_type2 = ARR_ELEMTYPE(array2);
-    const char *opname = "+";
-    Oid operator_oid;
-    
-    int nitems1;
-    int nitems2;
+    Oid element_type2 = ARR_ELEMTYPE(array2);   
+    Oid rtype; 
+    int nitems1, nitems2;
+    int nelems, n;
+    bits8 *bitmap1, *bitmap2;
+    int bitmask1, bitmask2;
+    FmgrInfo operfmgrinfo;
+    TypeCacheEntry *info1, *info2, *tinfo;
 
-    // extern Oid      OpernameGetOprid(List *names, Oid oprleft, Oid oprright);
-    // extern bool OperatorIsVisible(Oid oprid);
-    
-    //opertup = SearchSysCache1(OPEROID, ObjectIdGetDatum(oprid));
-    // if (HeapTupleIsValid(opertup))
-    //             Form_pg_operator operform = (Form_pg_operator) GETSTRUCT(opertup);
-    //    
-    // functionId = operform->oprcode;
-    
-    
-    /*
-     * This routine fills a FmgrInfo struct, given the OID
-     * of the function to be called.
-     */
-    // extern void fmgr_info(Oid functionId, FmgrInfo *flinfo);
+    /* Only 1D arrays for now */
+    if ( ndims1 != 1 || ndims2 != 1 )
+    {
+        elog(ERROR, "only 1-dimensional arrays supported");
+        return NULL;
+    }
 
-    // #define FunctionCall1(flinfo, arg1) \
+    /* What function works for these input types? Populate operfmgrinfo. */
+    arraymath_fmgrinfo_from_optype(opname, element_type1, element_type2, &operfmgrinfo);
+
+    /* What data type will the output array be? */
+    rtype = get_fn_expr_rettype(&operfmgrinfo);
+    tinfo = arraymath_typentry_from_type(rtype);
     
+    /* How big is the output array? */
+    nitems1 = ArrayGetNItems(ndims1, dims1);
+    nitems2 = ArrayGetNItems(ndims2, dims2);
+    nelems = Max(nitems1, nitems2);
+
+    /* If either input is empty, return empty */
+    if ( nitems1 == 0 || nitems2 == 0 )
+    {
+        return construct_empty_array(rtype);
+    }
     
-    operator_oid = OpernameGetOprid(list_make1(opname), element_type1, element_type2);
+    /* Allocate space for output data */
+    elems = palloc(sizeof(Datum)*nelems);
+    nulls = palloc(sizeof(bool)*nelems);
+
+    /* Learn more about the input arrays */
+    info1 = arraymath_typentry_from_type(element_type1);
+    info2 = arraymath_typentry_from_type(element_type2);
     
+    /* Loop over all the items, re-using items from the shorter */
+    /* array to apply to the longer */
+    for( n = 0; n < nelems; n++ )
+    {
+        Datum elt1, elt2;
+        int i1 = n % nitems1;
+        int i2 = n % nitems2;
+        bool isnull1, isnull2;
+
+        /* Initialize array pointers at start of loop, and */
+        /* on wrap-around */
+        if ( i1 == 0 )
+        {
+            ptr1 = ARR_DATA_PTR(array1);
+            bitmap1 = ARR_NULLBITMAP(array1);
+            bitmask1 = 1;
+        }
+        
+        if ( i2 == 0 )
+        {
+            ptr2 = ARR_DATA_PTR(array2);
+            bitmap2 = ARR_NULLBITMAP(array2);
+            bitmask2 = 1;
+        }
+        
+        /* Check null status */
+        isnull1 = BITMAP_ISNULL(bitmap1, bitmask1);
+        isnull2 = BITMAP_ISNULL(bitmap2, bitmask2);
+        
+        /* Start with NULL values */
+        elt1 = elt2 = (Datum) 0;
+        
+        if ( ! isnull1 )
+        {
+            /* Read the element value */
+            elt1 = fetch_att(ptr1, info1->typbyval, info1->typlen);
+
+            /* Move the pointer forward */
+            ptr1 = att_addlength_pointer(ptr1, info1->typlen, ptr1);
+            ptr1 = (char *) att_align_nominal(ptr1, info1->typalign);
+        }
+
+        if ( ! isnull2 )
+        {
+            /* Read the element value */
+            elt2 = fetch_att(ptr2, info2->typbyval, info2->typlen);
+
+            /* Move the pointer forward */
+            ptr2 = att_addlength_pointer(ptr2, info2->typlen, ptr2);
+            ptr2 = (char *) att_align_nominal(ptr2, info2->typalign);
+        }
+
+        /* NULL on either side of operator yields output NULL */
+        if ( isnull1 || isnull2 )
+        {
+            nulls[n] = true;
+            elems[n] = (Datum) 0;
+        }
+        else
+        {
+            nulls[n] = false;
+            elems[n] = FunctionCall2(&operfmgrinfo, elt1, elt2);
+        }
+        
+        BITMAP_INCREMENT(bitmap1, bitmask1);
+        BITMAP_INCREMENT(bitmap2, bitmask2);
+    }
+
+    /* Build 1-d output array */
+	dims[0] = nelems;
+	lbs[0] = 1;
+    array_out = construct_md_array(elems, nulls, 1, dims, lbs, rtype, tinfo->typlen, tinfo->typbyval, tinfo->typalign);
     
+    /* Output is supposed to be a copy, so free the inputs */
+    pfree(elems);
+    pfree(nulls);
+    
+    /* Make sure we haven't been given garbage */
+    if ( ! array_out )
+    {
+        elog(ERROR, "unable to construct output array");
+        return NULL;
+    }
+    
+    return array_out;
+    
+}
+
+/*
+* Our only function, takes in two arrays and tries to add
+* them together.
+*/
+Datum arr_plus_arr(PG_FUNCTION_ARGS);
+PG_FUNCTION_INFO_V1(array_add);
+Datum arr_plus_arr(PG_FUNCTION_ARGS)
+{
+    ArrayType *array1 = PG_GETARG_ARRAYTYPE_P(0);
+    ArrayType *array2 = PG_GETARG_ARRAYTYPE_P(1);
+//    Oid collation = PG_GET_COLLATION();
+
+    PG_RETURN_ARRAYTYPE_P(arraymath_array_oper_array(array1, "+", array2));
+}
+
+
+#if 0
+/*
+ * construct_array	--- simple method for constructing an array object
+ *
+ * elems: array of Datum items to become the array contents
+ *		  (NULL element values are not supported).
+ * nelems: number of items
+ * elmtype, elmlen, elmbyval, elmalign: info for the datatype of the items
+ *
+ * A palloc'd 1-D array object is constructed and returned.  Note that
+ * elem values will be copied into the object even if pass-by-ref type.
+ *
+ * NOTE: it would be cleaner to look up the elmlen/elmbval/elmalign info
+ * from the system catalogs, given the elmtype.  However, the caller is
+ * in a better position to cache this info across multiple uses, or even
+ * to hard-wire values if the element type is hard-wired.
+ */
+ArrayType *
+construct_array(Datum *elems, int nelems,
+				Oid elmtype,
+				int elmlen, bool elmbyval, char elmalign)
+#endif
+
+
+
+
+#if 0
     TypeCacheEntry *typentry;
     int                     typlen;
     bool            typbyval;
@@ -168,7 +420,7 @@ Datum array_add(PG_FUNCTION_ARGS)
                         bool            oprresult;
 
                         /* Get elements, checking for NULL */
-                        if (bitmap1 && (*bitmap1 & bitmask) == 0)
+                        if ()
                         {
                                 isnull1 = true;
                                 elt1 = (Datum) 0;
@@ -238,9 +490,8 @@ Datum array_add(PG_FUNCTION_ARGS)
 
                                     PG_RETURN_BOOL(result);
                             }
+#endif
 
-
-}
 	
 	
 
